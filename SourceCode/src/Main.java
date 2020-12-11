3a5af20ecae661447ad3c8db3c6f74a5668408e6
Molly Sugarman <mshuggs101@gmail.com>
2020-12-11 13:02:02 -0500

    update

--- SourceCode/src/Main.java
+++ SourceCode/src/Main.java
@@ -8,9 +8,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Game newGame = new Game(); // Declaring a new game
-        //newGame.playGame(); // Starting the new game
         int color = 1, add = 1; // "COLOR" also tell whos turn it is
+        String forfeit = "F";
         Players player[];
         player = new Players[2]; // Declaring the players
         player[0] = new Players(1, "b", 0, 0);
@@ -40,44 +39,45 @@ public class Main {
 
             int playsleft = 60;
 
-            while (playsleft > 0) { // PLAYER 1's turn
-                if (color == 1) {
-                    System.out.print("PLAYER 1's TURN [BLACK]: ");
-                }
-                if (color == 2) { // PLAYER 2's turn
-                    System.out.print("PLAYER 2's TURN [WHITE]: ");
-                }
-//            System.out.print("\n     Enter the x-coordinate of desired move [BETWEEN 0 AND 7]: ");
-                System.out.print("\n     Enter x-coordinate and y-coordinate [Press 'enter' after each value]: \n     ");
-                int row = input.nextInt();
-                System.out.print("\n     ");
-//            System.out.print("\n     Enter the y-coordinate of desired move [BETWEEN 0 AND 7]: ");
-                int col = input.nextInt();
-                col = 7 - col;
-                add = board.add(row, col, color);
-                if (add == -1) { //note: should prompt same player to move again if unsuccessful
-                    System.out.println("          .\n          .\n          .\n     Unsuccessful add: invalid move \n");
-                } else {
-                    System.out.println("          .\n          .\n          .\n     Successful add! New board state \n");
-                    //player[color].addPlayerCount();
+            while (playsleft > 0 || input.next().equals("F")) {
 
-                    //System.out.println("     Player " + color +" has now flipped a total of " + player[color].getPlayerCount() + " pieces \n ");
-                    // right now player count only flips 1 piece; need to figure out that if more than 1 piece is flipped how to tell how many pieces were flipped
-
-
-                    System.out.println("---UPDATED BOARD---");
-                    board.printBoard();
-                    // Swapping turns
-                    if (color == 1) {
-                        color = 2;
-                    } else {
-                        color = 1;
+                 if (color == 1) {
+                        System.out.print("PLAYER 1's TURN [BLACK]: ");
                     }
-                    playsleft--;
-                }
-                System.out.println();
+                    if (color == 2) { // PLAYER 2's turn
+                        System.out.print("PLAYER 2's TURN [WHITE]: ");
+                    }
+//            System.out.print("\n     Enter the x-coordinate of desired move [BETWEEN 0 AND 7]: ");
+                    System.out.print("\n     Enter x-coordinate and y-coordinate [Press 'enter' after each value]: \n     ");
+                    int row = input.nextInt();  // int row = input.nextInt();
+                    System.out.print("\n     ");
+//            System.out.print("\n     Enter the y-coordinate of desired move [BETWEEN 0 AND 7]: ");
+                    int col = input.nextInt();
+                    col = 7 - col;
+                    add = board.add(row, col, color);
+                    if (add == -1) { //note: should prompt same player to move again if unsuccessful
+                        System.out.println("          .\n          .\n          .\n     Unsuccessful add: invalid move \n");
+                    } else {
+                        System.out.println("          .\n          .\n          .\n     Successful add! New board state \n");
+                        //player[color].addPlayerCount();
 
+                        //System.out.println("     Player " + color +" has now flipped a total of " + player[color].getPlayerCount() + " pieces \n ");
+                        // right now player count only flips 1 piece; need to figure out that if more than 1 piece is flipped how to tell how many pieces were flipped
+
+
+                        System.out.println("---UPDATED BOARD---");
+                        board.printBoard();
+                        // Swapping turns
+                        if (color == 1) {
+                            color = 2;
+                        } else {
+                            color = 1;
+                        }
+                        playsleft--;
+                    }
             }
+
+
             int winner = board.getWinner();
             System.out.println("-- GAME OVER --");
             if (winner == 1) {
@@ -90,10 +90,20 @@ public class Main {
             System.out.println("---FLIPS---");
             System.out.println("   BLACK [PLAYER 1]: " + player[1].getFlipCount());
             System.out.println("   WHITE [PLAYER 2]: " + player[2].getFlipCount());
-
     }
 }
 
+/*
+do {
+            forfeit = input.next();
+            System.out.println("Please press q to quit");
+            }while (forfeit != "f" || forfeit !="F") {
+            System.out.println();
+            System.out.println("Player " + color + " has forfeited this game");
+            }
+
+            still firuging out the forfeit!
+ */
 
 
 
